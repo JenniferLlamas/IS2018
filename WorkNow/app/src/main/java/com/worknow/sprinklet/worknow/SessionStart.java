ddf7f1a129fa4ca1143a2f0abe63f9b440b75cd3
Jennifer Llamas <petra15>
2018-04-09 23:04:38 -0700

    app parte de virrey

---
+++ WorkNow/app/src/main/java/com/worknow/sprinklet/worknow/SessionStart.java
@@ -0,0 +1,13 @@
+package com.worknow.sprinklet.worknow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SessionStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_session_start);
+    }
+}
